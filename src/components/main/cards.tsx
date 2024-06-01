65a9f85e5ad689b38e6a3bba13cedb53ca6fa019
Elgius <114131653+Elgius@users.noreply.github.com>
2024-06-01 16:12:19 +0500

    lotties added + cards are functional

--- src/components/main/cards.tsx
+++ src/components/main/cards.tsx
@@ -1,12 +1,61 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import { NewsAlerts } from "@/lib/data";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "../ui/card";
+import { Label } from "../ui/label";
+import { Button } from "../ui/button";
+import { Player } from "@lottiefiles/react-lottie-player";
+import animation from "@/lib/lottie/2.json";
 
-function cards() {
+function Cards() {
+  // for api purposes
+  //   const [loading, setLoading] = useState(false);
+  //   const [data, setData] = useState([]);
+
+  //   if (loading) {
+  //     return (
+  //       <div>
+  //         <div>
+  //           <Player autoplay loop src={animation} className="w-[320px]"></Player>
+  //         </div>
+  //         <div className="text-center">Loading .....</div>
+  //       </div>
+  //     );
+  //   }
+
   return (
-    <div>
-      <div>hello world</div>
+    <div className="grid sm:grid-col md:grid-cols-2 lg:grid-cols-3 gap-10">
+      {NewsAlerts.map((newsItem) => (
+        <div key={newsItem.id}>
+          <Card className="w-[350px] shadow-lg border-solid ">
+            <CardHeader>
+              <CardTitle>{newsItem.title}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="grid items-end justify-center grid-cols gap-7">
+                <p>{newsItem.description}</p>
+              </div>
+            </CardContent>
+            <CardFooter className="flex flex-col gap-4">
+              <Button>Source</Button>
+              <Label>
+                {newsItem.alert ? (
+                  <p className="text-red-800 font-Poppins font-bold">Alert</p>
+                ) : null}
+              </Label>
+              <Label>Bias rating: {newsItem.bias}</Label>
+            </CardFooter>
+          </Card>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default cards;
+export default Cards;
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,80 +1,80 @@
 interface News {
-  id: Number;
+  id: string;
   title: string;
   description: string;
   alert: boolean;
-  bias: number;
+  bias: string;
 }
 
-const alers: News[] = [
+export const NewsAlerts: News[] = [
   {
-    id: 1,
+    id: "1",
     title: "First Alert",
     description: "This is the first alert message.",
     alert: true,
-    bias: 5,
+    bias: "5",
   },
   {
-    id: 2,
+    id: "2",
     title: "Second Alert",
     description: "This is the second alert message.",
     alert: false,
-    bias: 7,
+    bias: " 7",
   },
   {
-    id: 3,
+    id: "3",
     title: "Third Alert",
     description: "This is the third alert message.",
     alert: true,
-    bias: 9,
+    bias: "9",
   },
   {
-    id: 4,
+    id: "4",
     title: "Fourth Alert",
     description: "This is the fourth alert message.",
     alert: false,
-    bias: 6,
+    bias: "6",
   },
   {
-    id: 5,
+    id: "5",
     title: "Fifth Alert",
     description: "This is the fifth alert message.",
     alert: true,
-    bias: 8,
+    bias: "8",
   },
   {
-    id: 6,
+    id: "6",
     title: "Sixth Alert",
     description: "This is the sixth alert message.",
     alert: false,
-    bias: 10,
+    bias: "10",
   },
   {
-    id: 7,
+    id: "7",
     title: "Seventh Alert",
     description: "This is the seventh alert message.",
     alert: true,
-    bias: 11,
+    bias: "11",
   },
   {
-    id: 8,
+    id: " 8",
     title: "Eighth Alert",
     description: "This is the eighth alert message.",
     alert: false,
-    bias: 12,
+    bias: "12",
   },
   {
-    id: 9,
+    id: "9",
     title: "Ninth Alert",
     description: "This is the ninth alert message.",
     alert: true,
-    bias: 13,
+    bias: "13",
   },
   {
-    id: 10,
+    id: "10",
     title: "Tenth Alert",
     description: "This is the tenth alert message.",
     alert: false,
-    bias: 14,
+    bias: "14",
   },
 ];
