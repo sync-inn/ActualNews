3ae28e0b20fa33a5d9642e82ce35db6de4c55c07
Elgius <114131653+Elgius@users.noreply.github.com>
2024-06-23 23:51:52 +0500

    prod build

--- src/components/main/cards.tsx
+++ src/components/main/cards.tsx
@@ -32,7 +32,9 @@ function Cards() {
     const fetchData = async () => {
       console.log("Data fetching began");
       try {
-        const response = await fetch("http://localHost:3001/get/grabNews");
+        const response = await fetch(
+          "https://server.wheredoc.org/get/grabNews"
+        );
         const data = await response.json();
         setData(data);
         console.log(data);
